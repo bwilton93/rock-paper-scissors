b8d0c4292736d4e45357a1dd0360d71779b22aff
Barney W <B.Wilton1993@gmail.com>
2021-07-08 12:58:29 +0100

    Added function that creates display divs on page load

--- script.js
+++ script.js
@@ -4,12 +4,36 @@ let result;
 let playerScore = 0;
 let computerScore = 0;
 
+document.body.onload = resultsElement;
+
 document.getElementById('rock').onclick = play;
 document.getElementById('paper').onclick = play;
 document.getElementById('scissors').onclick = play;
 
+function resultsElement() {
+    let resultsDiv = document.createElement('div');
+    resultsDiv.setAttribute('class', 'results-container');
+    
+    let computerResultDiv = document.createElement('div');
+    computerResultDiv.setAttribute('id', 'computer-result');
+    computerResultDiv.innerHTML = '';
+    
+    let vsDiv = document.createElement('div');
+    vsDiv.setAttribute('id', 'vs-div');
+    vsDiv.innerHTML = '';
+    
+    let playerResultDiv = document.createElement('div');
+    playerResultDiv.setAttribute('id', 'player-result');
+    playerResultDiv.innerHTML = '';
+
+    resultsDiv.appendChild(playerResultDiv);
+    resultsDiv.appendChild(vsDiv);
+    resultsDiv.appendChild(computerResultDiv);
+
+    document.body.appendChild(resultsDiv);
+}
+
 function play(){
-    result = '';
     computerSelection = computerPlay();
     playerSelection = this.id;
     console.log('User: ' + playerSelection);
@@ -19,7 +43,6 @@ function play(){
     console.log('Player score: ' + playerScore);
     console.log('Computer score: ' + computerScore);
 }
-
 
 function computerPlay() {
     let arr = ['rock', 'paper', 'scissors']; // Initalise array with possible moves
